12edcf5a2f09c56831383c8071a865d8aec275b4
Mychael Contreras <miketw1212@hotmail.com>
2022-10-27 16:58:41 -0700

    updated api routes and added indexes to them along with models file

--- models/workout.js
+++ models/workout.js
@@ -2,48 +2,42 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+class Workout extends Model {
+
 }
 
-User.init(
+Workout.init(
     {
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
-          },
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-              isEmail: true,
-            },
-          },
-          password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              len: [8],
-            },
-          },
         },
-        {
-            hooks: {
-                beforeCreate: async (newUserData) => {
-                    newUserData.password = await bcrypt.hash
-                    (newUserData.password, 10);
-        return newUserData;
-      },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: 'user',
+            referencesKey: 'id',
+            foreignKey: true,
+        },
+        createdAt: {
+            type: Sequelize.DATE,
+            field: 'created_at',
+        },
+        updatedAt: {
+            type: Sequelize.DATE,
+            field: 'updated_at'
+        }
     },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
+    {
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'workout',
+    }
 );
+
+module.exports = Workout;
