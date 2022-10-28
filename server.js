57e2b5901f8274f895579a108584718351ec6d55
daniel-valean <dannyvalean@gmail.com>
2022-10-28 11:42:34 -0700

    connected login to add-workout, adjusted password database

--- server.js
+++ server.js
@@ -3,19 +3,13 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 //const helpers = require('./utils/helpers'); - we will decide
-
-const sequelize = require('./config/connection');
-
-// Create a new sequelize store using the express-session package
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// Configure and link a session object with the sequelize store
 const sess = {
   secret: 'Super secret secret', 
-
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -29,7 +23,6 @@ app.use(session(sess));
 const hbs = exphbs.create({ });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
