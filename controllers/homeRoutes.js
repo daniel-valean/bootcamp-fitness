4e27cc24647e6b670c662a8922654dcef4c50162
daniel-valean <dannyvalean@gmail.com>
2022-10-27 17:06:26 -0700

    routes and login setup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,14 @@
 //res render// 
+const router = require("express").Router()
+router.get("/",(req, res)=>{
+    res.render("home")
+})
+
+router.get("/login", (req, res)=>{
+    if(req.session.loggedIn){
+        res.redirect("/")
+        return 
+    }
+    res.render("login",{layout:"main"})
+})
+module.exports = router
