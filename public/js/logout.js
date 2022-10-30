55ef9e96520f067a23311a31f47b3adc405ddf26
BRITTANY BLALOCK <brittany.blalock2@gmail.com>
2022-10-29 21:31:19 -0700

    Updated so login button, logout button & signup work together.

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,13 +1,14 @@
-function logout() {
+function logoutHandler() {
     fetch("/api/user/logout", {
-      method: "post",
-      headers: { "Content-Type": "application/json" }
+        method: "post",
+        headers: {"Content-Type": "application/json"}
     })
-      .then(function() {
-        document.location.replace("/");
-      })
-      .catch(err => console.log(err));
-  }
-  
-  document.querySelector("#logout").addEventListener("click", logout);
-    
+        .then(function () {
+            document.location.replace("/", "/login");
+        })
+        .catch(err => console.log(err));
+}
+
+document
+    .querySelector("#logout-btn")
+    ?.addEventListener("click", logoutHandler);
